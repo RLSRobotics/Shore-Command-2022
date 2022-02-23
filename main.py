ee3325272787a6f6ea2b2945a28f55957f020440
Ryan Marquess <ryanjmar@outlook.com>
2022-02-22 20:14:09 -0800

    Fix main.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import cv2
-#import threading
 
 cam1 = cv2.VideoCapture(0)
 cam2 = cv2.VideoCapture(1)
 
-
 while(True):
-    #ret1, frame1 = cam1.read()
+    ret1, frame1 = cam1.read()
     ret2, frame2 = cam2.read()
-"""
-    if not (ret1 and ret2):
-        print(":(")
-        break
-"""
-    #cv2.imshow('cam1', frame1)
-    cv2.imshow('cam2', frame2)
 
-    cv2.waitKey(0);
+    if ret1:
+        cv2.imshow('cam1', frame1)
 
+    if ret2:
+        cv2.imshow('cam2', frame2)
+
+    cv2.waitKey(1)
 
 cam1.release()
 cam2.release()
