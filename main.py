dfa271ce532786f8c8a01acb333aa07c22a87306
Ryan Marquess <ryanjmar@outlook.com>
2022-02-23 11:43:25 -0800

    Implement threading for cameras

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import cv2
+import threading
 
-cam1 = cv2.VideoCapture(0)
-cam2 = cv2.VideoCapture(1)
+class Camera:
 
-while(True):
-    ret1, frame1 = cam1.read()
-    ret2, frame2 = cam2.read()
+    def __init__(self, id):
+        self.id = id
+        self.name = "cam" + str(id)
+        self.cam = cv.VideoCapture(id)
 
-    if ret1:
-        cv2.imshow('cam1', frame1)
+        self._display = threading.Thread(target=_updateFrame)
+        self._display.daemon = True
+        self._display.start();
 
-    if ret2:
-        cv2.imshow('cam2', frame2)
+    def _updateFrame(self):
 
-    cv2.waitKey(1)
+        while True:
+            ret, frame = self.cam.read()
 
-cam1.release()
-cam2.release()
-cv2.destroyAllWindows()
+            if ret:
+                cv2.imshow(self.name, frame)
+
+            cv2.waitKey(1)
+
+if __name__ == "__main__":
+    cam1 = Camera(0)
+    cam2 = Camera(1)
